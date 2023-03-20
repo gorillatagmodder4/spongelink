ccad49dcb77066305b748f155047fa1545730f4c
gorillatagmodder4 <91854621+gorillatagmodder4@users.noreply.github.com>
2023-03-19 20:04:02 -0400

    js

---
+++ js/fullscreen.js
@@ -0,0 +1,17 @@
+document.addEventListener("keydown", function(event) {
+  if (event.code === "KeyF") {
+    fullscreen();
+  }
+});
+function fullscreen() {
+  var iframe = document.getElementById("game-frame");
+  if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.webkitRequestFullscreen) {
+    iframe.webkitRequestFullscreen();
+  } else if (iframe.mozRequestFullScreen) {
+    iframe.mozRequestFullScreen();
+  } else if (iframe.msRequestFullscreen) {
+    iframe.msRequestFullscreen();
+  }
+}
